974fdf16ee226166d542f58a313e056b0ea629ab
minchoi <minchoi@DESKTOP-5FGGPIB.dhcp.wustl.edu>
2017-10-09 17:24:49 -0500

    Kim Choi Choi Studio 6

---
+++ debounce/debounce.ino
@@ -0,0 +1,34 @@
+const int buttonPin = 3;
+int counter = 0;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void buttonPressed() {
+  if((millis() - lastDebounceTime) > debounceDelay) {
+    counter = counter + 1;
+    Serial.println("Interrupt");
+  }
+  lastDebounceTime = millis();
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals
+  // Three edge types are supported:
+  //    CHANGE (when the status changes), RISING (button unpressed), FALLING (button pressed)
+  //    LOW (when the signal is LOW)
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, LOW);
+}
+
+void loop() {
+//  for(int i = 0; i < 100; i++) {
+//    Serial.println(i);
+//    delay(1000);
+//  }
+//  Serial.println(counter);
+//  delay(1000);
+}
+
